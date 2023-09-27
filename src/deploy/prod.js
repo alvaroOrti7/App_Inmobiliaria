5b54343cc2eccd0f163441a0dfe73fa632d88cf6
Alvaro <alvaroorti7@gmail.com>
2023-09-27 17:21:14 +0200

    second commit

---
+++ src/deploy/prod.js
@@ -0,0 +1,14 @@
+import moment from 'moment';
+import { execSync } from 'child_process';
+
+const dateTime = moment().format("MM/DD/YYYY HH:mm:ss");
+
+try {
+  execSync(
+    `cd ../form-topsolid-deploy && git checkout master &&  cp -r ../form-topSolid/dist/. ./ && git add . && git commit -m "Send to master at ${dateTime}" && git push origin master --force`,
+    { stdio: [0, 1, 2] }
+  );
+  console.log("Envio Correcto");
+} catch (err) {
+  console.log("Error. Nada que hacer");
+}
