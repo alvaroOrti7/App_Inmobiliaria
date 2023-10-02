aa894d4a181152959098f954d247f535cdc3bee1
Álvaro Ortí Segovia <alvaroorti7@gmail.com>
2023-10-02 18:06:01 +0200

    prueba11

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 export default {
   content: [
     "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
     './pages/**/*.{html,js,ts,jsx,tsx}',
-    './components/**/*.{html,js,ts,jsx,tsx}',
+    './components/**/*.{html,js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
