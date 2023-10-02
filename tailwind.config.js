4cb815d9f1871971def7d270d203a087a80daf55
Álvaro Ortí Segovia <alvaroorti7@gmail.com>
2023-10-02 17:47:33 +0200

    prueba11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    colors: {
-      white: '#F6FAFD',
-      red: '#E42024',
-      gray: '#E7E7E7',
-      border: 'rgb(82, 82, 85)',
-      black: '#3D3838',
-    },
-    backgroundImage: {
-      'fondo': "url('/fondo.webp')",
-      'banner': "url('/banner.webp')"
-    },
-    fontFamily: {
-      'catamaran': "'Catamaran', sans-serif",
-      'impact': "'Anton', sans-serif;"
-    },
-    boxShadow: {
-      'sombra': '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)'
-    },
-    extend: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  extend: {
+    theme: {
+      colors: {
+        white: "#F6FAFD",
+        red: "#E42024",
+        gray: "#E7E7E7",
+        border: "rgb(82, 82, 85)",
+        black: "#3D3838",
+      },
+      backgroundImage: {
+        fondo: "url('/fondo.webp')",
+        banner: "url('/banner.webp')",
+      },
+      fontFamily: {
+        catamaran: "'Catamaran', sans-serif",
+        impact: "'Anton', sans-serif;",
+      },
+      boxShadow: {
+        sombra: "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
+      },
       screens: {
-        'mobile': '280px',
-        'mobile-steelArrows': '294px',
-        'mobile-steelTittle': '378px',
+        mobile: "280px",
+        "mobile-steelArrows": "294px",
+        "mobile-steelTittle": "378px",
       },
     },
     plugins: [],
-  }
-}
+  },
+};
