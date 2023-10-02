6469ad802fcfc9a07f99dcd400b4cce2f021cd6a
Álvaro Ortí Segovia <alvaroorti7@gmail.com>
2023-10-02 17:12:26 +0200

    prueba7

--- src/components/banner/banner2.jsx
+++ src/components/banner/banner2.jsx
@@ -7,7 +7,7 @@ const Banner2 = () => (
                 DOMINA EL SECTOR METALÚRGICO CON TOPSOLID'STEEL
             </p>
         </div>
-        <img className="w-full h-[280px] object-cover" src="src/assets/hero6.webp" alt="" />
+        <img className="w-full h-[280px] object-cover" src="/hero6.webp" alt="" />
         <div className="absolute right-[170px] top-[100px] mobile:right-1 mobile:top-[120px] sm:right-16 sm:top-24 md:right-28 lg:right-36 xl:right-52 2xl:right-72">
             <Boton anchoSm="[190px]" alturaSm="[90px]" altura="[50px]" ancho="[130px]" />
         </div>
