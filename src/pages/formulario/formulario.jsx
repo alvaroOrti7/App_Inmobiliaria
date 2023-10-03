19756461b26dff1cfe3d44bd366296830e7dcf51
Álvaro Ortí Segovia <alvaroorti7@gmail.com>
2023-10-03 18:03:02 +0200

    prueba

--- src/pages/formulario/formulario.jsx
+++ src/pages/formulario/formulario.jsx
@@ -1,31 +1,30 @@
-import { useEffect, useRef } from 'react';
-import './Formulario.css';
-import Header from './header/header';
+import { useEffect, useRef } from "react";
+import "./Formulario.css";
+import Header from "./header/header";
 
 const Formulario = () => {
-    const formularioRef = useRef(null);
+  const formularioRef = useRef(null);
 
-    useEffect(() => {
-        if (formularioRef.current) {
-            hbspt.forms.create({
-                region: "eu1",
-                portalId: "139725651",
-                formId: "8f46d1de-7a7a-42bf-80e2-ebc8cc992081",
-                target: '#formulario-container',
-              });
-        }
-    }, []);
+  useEffect(() => {
+    if (formularioRef.current) {
+      hbspt.forms.create({
+        region: "eu1",
+        portalId: "139725651",
+        formId: "8f46d1de-7a7a-42bf-80e2-ebc8cc992081",
+        target: "#formulario-container",
+      });
+    }
+  }, []);
 
-    return (
-        <div>
-            <Header />
-            <h1 id="formu">Solicita una demo</h1>
-            <section>
-                <div id="formulario-container" ref={formularioRef}>
-                </div>
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <h1 id="formu">Solicita una demo</h1>
+      <section>
+        <div id="formulario-container" ref={formularioRef}></div>
+      </section>
+    </div>
+  );
 };
 
 export default Formulario;
